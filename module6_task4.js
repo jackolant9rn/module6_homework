ba4dbbff2604552378ba504668b0901fbdb61989
jackolant9rn <117020216+jackolant9rn@users.noreply.github.com>
2023-02-01 20:13:25 +0700

    Добавлена проверка агрументов функции

--- module6_task4.js
+++ module6_task4.js
@@ -1,12 +1,22 @@
 function numbersOutput(a, b) {
-    let i = a;
+  if (typeof a != 'number' || typeof b != 'number' || Number.isNaN(a) || Number.isNaN(b) || !Number.isInteger(a) || !Number.isInteger(b) || a > b) {
+    console.log('Данные неверны!');
+  } else {let i = a;
     const timerId = setInterval(function() {
       console.log(i);
-      if (i == b) {
-        clearInterval(timerId);
-      }
+        if (i == b) {
+          clearInterval(timerId);
+        }
       i++;
     }, 1000);
+  }
 }
     
 numbersOutput(5, 15);
+
+// Just a couple of tests
+
+/*numbersOutput(16, 15);
+numbersOutput(3.14, 15);
+numbersOutput(5, NaN);
+numbersOutput('5', 15);*/
